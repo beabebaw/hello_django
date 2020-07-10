130d86cacc0060f8b6f7b8d6add353e99f893b33
Bea <bea.a.ribeiro@hotmail.com>
2020-07-09 22:35:39 -0300

    Segundo commit da  primeira aplicacao django retornando hello world com coisas

--- core/views.py
+++ core/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def hello(request, nome, idade):
+def hello(request, nome, idade, soma, num1, num2):
+    soma = num1 + num2
+    multi = num1*num2
     return HttpResponse(
-        '<h1>Hello {} de {} anos de idade</h1>'
-        .format(nome, idade)
+        '<h1>Hello {} de {} anos de idade</h1><br>Seus números {} e {} somados são {}. Multiplicados, são {}.'
+        .format(nome, idade, num1, num2, soma, multi)
     )
