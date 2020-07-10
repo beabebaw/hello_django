88c53277d2db426689261a8f6f5b8a74c71a438d
Bea <bea.a.ribeiro@hotmail.com>
2020-07-09 22:03:45 -0300

    Commit da primeira aplicacao django retornando hello world

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def hello(request, nome, idade):
+    return HttpResponse(
+        '<h1>Hello {} de {} anos de idade</h1>'
+        .format(nome, idade)
+    )
